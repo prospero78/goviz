71d72e3366b0b4130c2ec9b0bd1df6438eef1f54
SVI <shipkovvi@atomsbt.ru>
2022-09-09 11:48:58 +0300

    Перенос файлов, обновление вендоринга

---
+++ examples/screen/main.go
@@ -0,0 +1,24 @@
+// package main -- показывает пример создания экрана
+//  Для запуска выполнить:
+//    go run ./examples/screen/main.go
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/prospero78/goviz/v1"
+)
+
+func main() {
+	screen, err := goviz.GetScreen()
+	if err != nil {
+		logrus.WithError(err).Errorln("screen.go.main(): in get screen")
+		os.Exit(1)
+	}
+	screen.Clear()
+	time.Sleep(time.Second * 3)
+	screen.Close()
+}
