d174609d1dd41fa982e3718e2f2a2e65f82cdc84
SVI <shipkovvi@atomsbt.ru>
2022-09-09 15:16:26 +0300

    Усиление модели ООП

--- v1/pos/pos.go
+++ v1/pos/pos.go
@@ -1,17 +1,43 @@
 package pos
 
-import "github.com/prospero78/goviz/v1/alias"
+import (
+	"github.com/prospero78/goviz/v1/alias"
+	"github.com/prospero78/goviz/v1/posx"
+	"github.com/prospero78/goviz/v1/posy"
+	"github.com/prospero78/goviz/v1/types"
+)
 
 // Pos -- позиция элемента на экране
 type Pos struct {
-	X alias.APosX // Позиция по X
-	Y alias.APosY // Позиция по Y
+	x types.IPosX // Позиция по X
+	y types.IPosY // Позиция по Y
 }
 
 // NewPos -- возвращает новую позицию объекта
 func NewPos(x alias.APosX, y alias.APosY) *Pos {
 	return &Pos{
-		X: x,
-		Y: y,
+		x: posx.NewPosX(x),
+		y: posy.NewPosY(y),
 	}
+}
+
+// Get -- возвращает позицию объекта
+func (sf *Pos) Get() (alias.APosX, alias.APosY) {
+	return sf.x.Get(), sf.y.Get()
+}
+
+// Set -- устанавливает позицию объекта
+func (sf *Pos) Set(x alias.APosX, y alias.APosY) {
+	sf.x.Set(x)
+	sf.y.Set(y)
+}
+
+// PosX -- возвращает объект положения по X
+func (sf *Pos)PosX()types.IPosX{
+	return sf.x
+}
+
+// PosY -- возвращает объект положения по Y
+func (sf *Pos)PosY()types.IPosY{
+	return sf.y
 }
--- v1/pos/pos_test.go
+++ v1/pos/pos_test.go
@@ -8,10 +8,21 @@ import "testing"
 
 func TestPos(t *testing.T) {
 	pos := NewPos(5, 6)
-	if pos.X != 5 {
-		t.Errorf("TstPos(): x(%v)!=5", pos.X)
+	x := pos.PosX().Get()
+	if x != 5 {
+		t.Errorf("TstPos(): x(%v)!=5", x)
 	}
-	if pos.Y != 6 {
-		t.Errorf("TstPos(): y(%v)!=6", pos.Y)
+	y := pos.PosY().Get()
+	if y != 6 {
+		t.Errorf("TstPos(): y(%v)!=6", y)
+	}
+
+	pos.Set(7, 8)
+	x,y = pos.Get()
+	if x != 7 {
+		t.Errorf("TstPos(): x(%v)!=7", x)
+	}
+	if y != 8 {
+		t.Errorf("TstPos(): y(%v)!=8", y)
 	}
 }
